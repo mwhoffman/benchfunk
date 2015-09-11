0245d38bc13a694a814ec10edd13522079481255
Bobak Shahriari <bshahr@cs.ubc.ca>
2015-09-10 21:44:17 -0700

    Fix plotting code.

--- benchfunk/core/plotters.py
+++ benchfunk/core/plotters.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
+import numpy as np
 from jug import TaskGenerator
-
-plt.rc('lines', linewidth=2)
+import ezplot
 
 
 __all__ = ['plot_stack']
@@ -12,27 +11,26 @@ def plot_stack(stack_results, problems=None, policies=None, name=''):
     problems = problems if problems is not None else stack_results.key()
     nfigs = len(problems)
 
-    fig, axs = plt.subplots(1, nfigs, sharey=True)
+    fig = ezplot.figure(figsize=(5*nfigs, 4))
 
-    for ax, expt in zip(axs, problems):
+    for i, expt in enumerate(problems):
         results = stack_results[expt]
         policies = policies if policies is not None else results.keys()
 
-        for policy in policies:
-            v = results[policy]
-            # TODO: Fix this mess. Due to having a list of structured arrays.
-            nreps = len(v)
-            mean = v[0][1]
-            for i in xrange(1, nreps):
-                mean += v[i][1]
-            mean /= nreps
+        ax = fig.add_subplot(1, nfigs, i+1)
 
-            ax.plot(mean, label=policy)
+        for policy in policies:
+            xbest, ybest = zip(*results[policy])
+            iters = np.arange(np.shape(ybest)[1])
+
+            mu = np.mean(ybest, axis=0)
+            std = np.std(ybest, axis=0)
+            ax.plot_banded(iters, mu, std, label=policy)
 
         ax.set_title(expt, fontsize=16)
 
-    axs[0].legend(loc=0, fontsize=16)
-    plt.savefig(name)
+    ax.legend(loc=0, fontsize=16)
+    ezplot.plt.savefig(name)
 
     return fig
 
