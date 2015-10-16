cee7aadae228fe016f3925e6559efec244394872
Bobak Shahriari <bshahr@cs.ubc.ca>
2015-10-16 12:07:26 -0700

    Update example script in light of new runners.

--- examples/example.py
+++ examples/example.py
@@ -1,35 +1,57 @@
-"""
-Example script which runs a number of policies on a single problem with varying
-levels of observation noise.
-"""
-
 from collections import OrderedDict
-from benchfunk.core import run_stack, plot_stack
+import itertools
+from benchfunk import functions
+from benchfunk.core import run_instance, run_stack
 
-from pybo.policies import *
-from benchfunk.functions import *
-
-# parameters
-name = __name__
-niter = 10
-nreps = 3
-
-# prescribe problem instances
-problems = OrderedDict()
-problems['Gramacy(0.01)'] = (Gramacy, dict(sn2=0.01))
-problems['Gramacy(0.05)'] = (Gramacy, dict(sn2=0.05))
-problems['Gramacy(0.10)'] = (Gramacy, dict(sn2=0.10))
+niter = 100
+nreps = 10
 
 # prescribe policies to try
-policies = OrderedDict()
-policies['EI(0.0)'] = (EI, dict(xi=0.0))
-policies['PI(0.1)'] = (PI, dict(xi=0.1))
-policies['TS(100)'] = (Thompson, dict(n=100))
+policies = [('EI', dict(xi=0.0)),
+            ('PI', dict(xi=0.1)),
+            ('PES', dict(opes=False)),
+            ('PES', dict(opes=True))]
 
-# run stack of experiments
-results = run_stack(problems, policies, niter, nreps, name=name)
-results.name = name
+# prescribe models to try
+models = [None]
 
-# plot results
-fig = plot_stack(results, problems.keys(), policies.keys(), name=name)
-fig.name = '.'.join([name, 'plot'])
+# prescribe functions to test on and experimental setups, in this case simply
+# noise levels
+funcs = [functions.Gramacy,
+         functions.Branin,
+         functions.Goldstein,
+         functions.Bohachevsky,
+         functions.Hartmann3,
+         functions.Hartmann6]
+
+setups = [dict(sn2=0.01),
+          dict(sn2=0.05),
+          dict(sn2=0.10)]
+
+problems = itertools.product(funcs, setups)
+
+# list all experiments and build stack
+experiments = itertools.product(problems, models, policies)
+stack = OrderedDict()
+
+for problem, model, policy in experiments:
+    name = format(problem[0](**problem[1]))
+    if name not in stack:
+        stack[name] = OrderedDict()
+
+    # name the particular run, in this example the relevant quantities are
+    # the policy and the model
+    key = str(policy[0])
+    key += '-{0}'.format(str(model)) if model is not None else ''
+
+    # the following kwargs must correspond to those required by the particular
+    # experiment to be run
+    stack[name][key] = dict(
+        problem=problem,
+        model=model,
+        policy=policy,
+        niter=niter,
+    )
+
+# run stack of tasks
+results = run_stack(run_instance, stack, nreps)
